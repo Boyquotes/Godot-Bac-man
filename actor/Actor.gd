cd474cfdf18dda006862584c45ae7532fbbd3eb0
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-02-24 13:04:14 +0000

    Simple screen-wrapping of Actors

--- actor/Actor.gd
+++ actor/Actor.gd
@@ -10,10 +10,8 @@ export var speed = 60
 
 var age = 0
 var facing = Facing.NONE setget set_facing
-
-
-func _ready():
-	$AnimatedSprite.play("default")
+onready var screen_width = get_viewport_rect().size.x
+onready var screen_height = get_viewport_rect().size.y
 
 
 func _process(delta):
@@ -30,18 +28,34 @@ func _process(delta):
 		Facing.NONE:
 			pass
 
+	# Modulus is a little awkward because floats, and also less flexible
+	if position.x >= screen_width:
+		position.x -= screen_width
+	elif position.x < 0:
+		position.x += screen_width
+
+	if position.y >= screen_height:
+		position.y -= screen_height
+	elif position.y < 0:
+		position.y += screen_height
+
 
 func set_facing(facing_):
 	match facing_:
 		Facing.UP:
+			$AnimatedSprite.play()
 			turn_up()
 		Facing.RIGHT:
+			$AnimatedSprite.play()
 			turn_right()
 		Facing.DOWN:
+			$AnimatedSprite.play()
 			turn_down()
 		Facing.LEFT:
+			$AnimatedSprite.play()
 			turn_left()
 		Facing.NONE:
+			$AnimatedSprite.stop()
 			turn_right()
 
 	facing = facing_
--- actor/Player.gd
+++ actor/Player.gd
@@ -2,10 +2,6 @@ class_name Player
 extends Actor
 
 
-func _process(_delta):
-	pass
-
-
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_up"):
 		set_facing(Facing.UP)
