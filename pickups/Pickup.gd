21965927e3cb7dc75efdd4b8ae30283cec85fdca
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-02-25 17:31:52 +0000

    Free Pickups after picking up

--- pickups/Pickup.gd
+++ pickups/Pickup.gd
@@ -8,3 +8,6 @@ onready var signal_name = "pickup_" + pickup_type
 
 func _on_Pickup_body_entered(body):
 	body.emit_signal(signal_name)
+	$AudioStreamPlayer.play()
+	$CollisionShape2D.set_deferred("disabled", true)
+	hide()
