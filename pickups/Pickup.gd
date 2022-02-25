d468924c50bba872bee8da3d6705195acde29373
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-02-25 16:45:42 +0000

    Cause Player to emit "pickup_pellet" signal, etc.

--- pickups/Pickup.gd
+++ pickups/Pickup.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
 
+export var pickup_type : String
+
+onready var signal_name = "pickup_" + pickup_type
+
+
 func _on_Pickup_body_entered(body):
-	print(body)
+	body.emit_signal(signal_name)
