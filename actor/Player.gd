9ec53869f2eb5a3d41fb42498c8359db5670dbf9
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-02-25 17:09:33 +0000

    Give Player the signals we want it to emit

--- actor/Player.gd
+++ actor/Player.gd
@@ -2,6 +2,12 @@ class_name Player
 extends Actor
 
 
+# warning-ignore: UNUSED_SIGNAL
+signal pickup_pellet
+# warning-ignore: UNUSED_SIGNAL
+signal pickup_big_pellet
+
+
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_up"):
 		queue_facing(Facing.UP)
