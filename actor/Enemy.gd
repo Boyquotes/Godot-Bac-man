c4fe50931e5fe06d762428a7a8f77de8ee15bd7c
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-02-26 16:04:21 +0000

    Wire up Enemies to Levels a bit differently

--- actor/Enemy.gd
+++ actor/Enemy.gd
@@ -2,9 +2,16 @@ class_name Enemy
 extends Actor
 
 
+signal request_path
+
+
+var nav_path = []
+var player : Player
+
+
 func _process(_delta):
 	pass
 
 
-func update_player_location(loc: Vector2):
-	print(loc)
+func _on_PathTimer_timeout():
+	emit_signal("request_path", self, player.position)
--- levels/LevelBase.gd
+++ levels/LevelBase.gd
@@ -8,7 +8,8 @@ export var enemy_start = Vector2(0,0)
 func _ready():
 	$Player.position = 16 * player_start + Vector2(16, 16)
 	$Enemy.position = 16 * enemy_start + Vector2(16, 16)
+	get_tree().call_group("enemies", "set", "player", $Player)
 
 
-func _on_EnemyNotifyTimer_timeout():
-	get_tree().call_group("enemies", "update_player_location", $Player.position)
+func _on_Enemy_request_path(enemy: Enemy, target: Vector2):
+	enemy.set("nav_path", [enemy.position, target])
