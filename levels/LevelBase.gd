ee0a5f2e297ff8bb622f59444a81cb160051c63e
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-02-26 10:47:49 +0000

    Stick an Enemy into levels

--- levels/LevelBase.gd
+++ levels/LevelBase.gd
@@ -2,7 +2,9 @@ extends TileMap
 
 
 export var player_start = Vector2(0,0)
+export var enemy_start = Vector2(0,0)
 
 
 func _ready():
 	$Player.position = 16 * player_start + Vector2(16, 16)
+	$Enemy.position = 16 * enemy_start + Vector2(16, 16)
