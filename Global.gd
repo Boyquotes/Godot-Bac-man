fcdafeb94fcf080e94e210f671e8134a41ef9550
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-02-28 18:12:35 +0000

    Generalise scene-switching

--- Global.gd
+++ Global.gd
@@ -1,8 +1,16 @@
 extends Node
 
 
+const scene_order = [
+	"TitleScreen",
+	"levels/level_test",
+]
+
 var player_lives = 3
+var score = 0
 var current_scene : Node = null
+var current_scene_index = 0
+
 onready var root = get_tree().get_root()
 
 
@@ -11,9 +19,14 @@ func _ready():
 
 
 func load_next_scene():
+	current_scene_index += 1
+	assert(
+			current_scene_index < scene_order.size(),
+			"Fell off the end of scene_order"
+	)
 	# warning-ignore: RETURN_VALUE_DISCARDED
-	get_tree().change_scene("res://levels/level_test.tscn")
-	init_current_scene()
+	get_tree().change_scene("res://%s.tscn" % scene_order[current_scene_index])
+	call_deferred("init_current_scene")
 
 
 func init_current_scene():
