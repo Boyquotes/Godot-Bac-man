b8a73f2899996a0113711fe6baac7b40effd31d3
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-02-28 18:49:08 +0000

    Have Pickups watch for area_entered, not body_entered

--- pickups/Pickup.gd
+++ pickups/Pickup.gd
@@ -6,8 +6,8 @@ export var pickup_type : String
 onready var signal_name = "pickup_" + pickup_type
 
 
-func _on_Pickup_body_entered(body):
-	body.emit_signal(signal_name)
+func _on_Pickup_area_entered(area):
+	area.owner.emit_signal(signal_name)
 	$AudioStreamPlayer.play()
 	$CollisionShape2D.set_deferred("disabled", true)
 	hide()
