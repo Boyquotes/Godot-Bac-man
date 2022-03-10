bc21ac139fbc1457701ff795f066d6faeeb94f5d
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-03-10 18:08:41 +0000

    Set project default font and (blank) theme

--- HUD.gd
+++ HUD.gd
@@ -10,7 +10,7 @@ func _ready():
 
 func update_label_text():
 	$Score.text = "Score: %d" % Global.score
-	$Lives.text = "×%d" % Global.player_lives
+	$Lives.text = "x%d" % Global.player_lives
 	$Level.text = "Level %d" % Global.current_scene_index
 
 
